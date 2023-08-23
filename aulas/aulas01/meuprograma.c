f0a809a4bf5a978062b8c55cbd7d5bf5d8a82335
Kauas1lva <142408379+Kauas1lva@users.noreply.github.com>
2023-08-23 13:56:24 +0000

    envio para o github

---
+++ aulas/aulas01/meuprograma.c
@@ -0,0 +1,15 @@
+//*********************
+//Autor: Kauã
+
+#include <stdio.h>
+#include <math.h>
+
+//funcao principal
+
+int main() {
+ printf("Ola pessoal!\n");
+ printf("Raiz quadrada de 2 é %f\n", sqrt(2));
+  printf("O Flamengo é o maior do Brasil!");
+  return 0;
+}
+
