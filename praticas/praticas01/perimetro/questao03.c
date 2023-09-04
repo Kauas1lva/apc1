a6f9bd046567321af5e954062ff6ce35425c8c19
Kauas1lva <142408379+Kauas1lva@users.noreply.github.com>
2023-09-04 13:58:39 +0000

    codigos3

---
+++ praticas/praticas01/perimetro/questao03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define PI 3.1416
+
+int main() {
+  int raio_pizza = 10;
+
+    float perimetro_pizza = 2 * PI * raio_pizza;
+
+printf("O perimetro da pizza é %f cm\n", perimetro_pizza);
+
+  return 0;
+  
+}
