d6ad1bd0fc86915ff59ec44520accdb9ba82d0e9
Kauas1lva <142408379+Kauas1lva@users.noreply.github.com>
2023-10-09 13:50:27 +0000

    aula de if

---
+++ praticas/praticas03/questao02.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+ float valor_bruto = 0.0f;
+
+  printf("Entre com o valor bruto:");
+  scanf("%f", &valor_bruto);
+
+  float valor_desconto = 0.0f;
+
+  int valor_bruto_ate_100 = valor_bruto <= 100.0f;
+
+  if (valor_bruto_ate_100) {
+    valor_desconto = valor_bruto * 0.01f;
+  }
+  int valor_bruto_entre_101_e_500 = valor_bruto > 100.0f && valor_bruto <= 500.0f
+
+    if (valor_bruto_entre_101_e_500) {
+    valor_desconto = valor_bruto * 0.05f;
+  }
+    int valor_bruto_acima_de_500 = valor_bruto > 500;
+
+    if (valor_bruto_acima_de_500) {
+      valor_desconto = valor_bruto * 0.1f;
+    }
+  return 0;
+}
