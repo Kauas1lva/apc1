b1168fd9288820e2c34bd572fa85fd9618fd083a
Kauas1lva <142408379+Kauas1lva@users.noreply.github.com>
2023-10-30 13:50:00 +0000

    aula matrizes

--- aulas/aulas05/menu.c
+++ aulas/aulas05/menu.c
@@ -3,32 +3,82 @@
 
 int main() {
   char opcao = 0;
-float saldo = 0.0f;
+  float saldo = 0.0f;
   
   while(1) {
     int ok = system("clear");
-    printf("MENU PRINCIPAL\n");
-    printf("1 - Consultar saldo\n");
-    printf("2 - Fazer recarga\n");
-    printf("3 - Listar recados\n");
-    printf("0 - Sair\n");
+    printf("        ___             __  __  ___        _  ___ __   _     \n");
+    printf(" )\\/)  )_  )\\ ) / /    )_) )_)  )  )\\ ) / `  )  )_) /_) ) \n");  
+    printf("(   (  (__ (  ( (_/    /   / \\ _(_ (  ( (_. _(_ /   / / (__ \n");
+    printf("▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄\n");
+    printf("█  1 - Consultar saldo                                     █\n");
+    printf("█  2 - Fazer recarga                                       █\n");
+    printf("█  3 - Listar recados                                      █\n"); 
+    printf("█  0 - Sair                                                █\n"); 
+    printf("█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█\n");
     printf("Sua escolha => ");
     ok = scanf("%c", &opcao);
     getchar();
 
     switch(opcao) {
-      case '1': printf("Seu saldo é %.2f\n", saldo); break;
-      case '2': printf("Escolha um valor de recarga:"); break;
-      case '3': printf("Voce nao tem recados\n"); break;
+      case '1':
+      ok = system("clear");
+      printf("Seu saldo é %.2f\n", saldo); break;
+      printf("CONSULTA SALDO\n");
+      printf("Seu saldo é %.2f\n", saldo); 
+      printf("Pressione ENTER para continuar...");
+      getchar();
+      break;
+      case '2': {
+        char valor = 0;
+        while (valor != '0') {
+          ok = system("clear");
+          printf("FAZER RECARGA\n");
+          printf("1 - R$ 10,00\n");
+          printf("2 - R$ 20,00\n");
+          printf("5 - R$ 50,00\n");
+          printf("Escolha um valor de recarga ou digite 0 para retornar: ");
+          ok = scanf("%c", &valor);
+          getchar();
+
+          switch(valor) {
+             case '1': saldo = saldo + 10.0f; break;
+             case '2': saldo = saldo + 20.0f; break;
+             case '5': saldo = saldo + 50.0f; break;
+             case '0': break;
+             default: 
+               printf("Opção inválida!\n"); 
+               printf("Pressione ENTER para continuar...");
+               getchar();
+               break;
+          }
+
+          if (valor == '1' || valor == '2' || valor == '5') {
+            printf("\e[0;32mRecarga realizada com sucesso!\e[0m\n");
+            printf("Pressione ENTER para continuar...");
+            getchar();
+          }
+        }
+        break;
+      }
+          
+      case '3': 
+      ok = system("clear");
+      printf("LISTAR RECADOS\n"); 
+      printf("Você não tem recados\n"); 
+      printf("Pressione ENTER para continuar...");
+      getchar();
+      break;
       case '0': break;
-      default: printf("Opcao invalida!\n"); break;
-    }
+            default: 
+              printf("\e[0;31mOpção inválida!\e[0m\n");
+              printf("Pressione ENTER para continuar...");
+              getchar();
+              break;
+          }
 
-    if(opcao == '0') break;
+          if (opcao == '0') break;
+        }
 
-    printf("Pressione ENTER para continuar...");
-    getchar();
-  }
-  
-return 0;
-}
+        return 0;
+      }
